841cec78029d5bf7c76320cec77243d981a06637
ruha-29m <haruka.a.kick@gmail.com>
2025-06-08 18:29:02 +0900

    Create Kato花子_Chapter18.java

---
+++ kadai_018/KatoHanako_Chapter18.java
@@ -0,0 +1,19 @@
+package kadai_018;
+
+public class KatoHanako_Chapter18 extends Kato_Chapter18{
+	
+	public KatoHanako_Chapter18(){
+		setGivenName();
+	}
+	public void setGivenName() {
+		// TODO 自動生成されたメソッド・スタブ
+		
+	
+		givenName = "花子";
+	 //名を表すフィールドの値をセットする
+	}
+	public void eachIntroduce() {	
+		System.out.println ("趣味は読書です");
+	}
+}
+ 
