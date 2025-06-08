608f5b2f0cc68fafabf5780f860bfb6d9905fede
ruha-29m <haruka.a.kick@gmail.com>
2025-06-08 23:35:14 +0900

    コードを編集

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -2,18 +2,12 @@ package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18{
 	
-	public KatoTaro_Chapter18(){
-		setGivenName();
-	}
 	public void setGivenName() {
-		// TODO 自動生成されたメソッド・スタブ
-		
-	
 		givenName = "太郎";
 	 //名を表すフィールドの値をセットする
 	}
 	public void eachIntroduce() {	
 		System.out.println ("Javaが得意です");
 	}
-
+	
 }
