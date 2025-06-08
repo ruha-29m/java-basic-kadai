c1b90eaf93122db42cde0325fc8ab2ceb0f685b5
ruha-29m <haruka.a.kick@gmail.com>
2025-06-08 23:34:06 +0900

    コードを編集

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -5,16 +5,18 @@ public class KatoExec_Chapter18{
 	public static void main(String[] args) {
 	
 	Kato_Chapter18 taro = new KatoTaro_Chapter18();
+	taro.setGivenName();
 	taro.execIntroduce();
 	System.out.println();
 	
 	Kato_Chapter18 ichiro = new KatoIchiro_Chapter18();
+	ichiro.setGivenName();
 	ichiro.execIntroduce();
 	System.out.println();
 	
 	Kato_Chapter18 hanako = new KatoHanako_Chapter18();
+	hanako.setGivenName();
 	hanako.execIntroduce();
 	System.out.println();
-
-}
+	}
 }
