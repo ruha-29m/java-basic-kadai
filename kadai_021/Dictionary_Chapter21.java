5e3286d450e9047e16ec8d3149c1efcac9c627d6
ruha-29m <haruka.a.kick@gmail.com>
2025-06-14 18:02:05 +0900

    コードを編集2

--- kadai_021/Dictionary_Chapter21.java
+++ kadai_021/Dictionary_Chapter21.java
@@ -1,44 +1,39 @@
 package kadai_021;
 
 import java.util.HashMap;
-import java.util.Scanner;
 
-public class Dictionary_Chapter21 {
-   public static void main(String[] args) {
-	   
-	  
-	   HashMap<String,String> Dictionary = new HashMap<String,String>();
-	  // 辞書に単語を追加
-      Dictionary.put("apple","りんご");
-      Dictionary.put("peach","桃");
-      Dictionary.put("banana","バナナ");
-      Dictionary.put("lemon","レモン");
-      Dictionary.put("pear","梨");
-      Dictionary.put("kiwi","キウィ");
-      Dictionary.put("strawberry","いちご");
-      Dictionary.put("grape","ぶどう");
-      Dictionary.put("muscat","マスカット");
-      Dictionary.put("cherry","さくらんぼ");
-	   }
-      
-      Scanner scanner = new Scanner(System.in);{
-      
-      while (true) {
-    	  System.out.println("調べる英単語を入力してください(終了するには'quit'を入力)：");
-      String input = scanner.nextLine();
-      
-      if (input.equalsIgnoreCase("quit")) {
-    	  break;
-      }
-      
-      if (Dictionary.containsKey(input)) {
-      System.out.println(input + "は「" +  Dictionary.get(input) + "」です。");
-      } else {
-    	  System.out.println(input +  "は辞書に存在しません。");
-      }
-   }
-      scanner.close();
-      
-      }
+public class Dictionary_Chapter21 {//クラス
+
+	// 辞書に単語を追加
+	public static void main(String[] args) {//メインメソッド
+	}
+
+	public static void Dictionary() {
+		HashMap<String, String> dictionary = new HashMap<>();
+
+		//10通りの英単語と意味
+		dictionary.put("apple", "りんご");
+		dictionary.put("peach", "桃");
+		dictionary.put("banana", "バナナ");
+		dictionary.put("lemon", "レモン");
+		dictionary.put("pear", "梨");
+		dictionary.put("kiwi", "キウィ");
+		dictionary.put("strawberry", "いちご");
+		dictionary.put("grape", "ぶどう");
+		dictionary.put("muscat", "マスカット");
+		dictionary.put("cherry", "さくらんぼ");
+
+		//調べる単語,配列
+		String[] wordArray = { "apple", "banana", "grape", "orange" };
+
+		for (String word : wordArray) {
+			if (dictionary.containsKey(word)) {
+				String meaning = dictionary.get(word);
+				System.out.println(word + "の意味は" + meaning);
+			} else {
+				// キーが存在しない場合
+				System.out.println(word + "は辞書に存在しません");
+			}
+		}
+	}
 }
-
