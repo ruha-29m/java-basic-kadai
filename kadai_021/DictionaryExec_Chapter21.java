5536d5c9d5bf109639f73693c9e09ed966c8a8e5
ruha-29m <haruka.a.kick@gmail.com>
2025-06-14 22:28:52 +0900

    コードを編集3

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -2,9 +2,9 @@ package kadai_021;
 
 public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
-
-		Dictionary_Chapter21.Dictionary();
-
+		//インスタンス
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+		
 	}
 
 }
