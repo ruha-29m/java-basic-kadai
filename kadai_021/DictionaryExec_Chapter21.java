00c4a4e204723df08492f92b29d35df40fa9968a
ruha-29m <haruka.a.kick@gmail.com>
2025-06-15 00:41:23 +0900

    コードの修正4

--- kadai_021/DictionaryExec_Chapter21.java
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -4,7 +4,8 @@ public class DictionaryExec_Chapter21 {
 	public static void main(String[] args) {
 		//インスタンス
 		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
-		
+		String[] wordsToCheck = { "apple", "banana", "grape", "orange" };
+		dictionary.referer(wordsToCheck);
 	}
 
 }
